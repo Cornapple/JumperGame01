07c94194d4a7a944a14f5362fa68a09c904036b5
DELILAH\corne <1031995@student.sae.edu.au>
2025-05-29 08:43:42 +1000

    further work on movement script. jumping now possible. double jump works but only once.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -12,16 +12,19 @@ public class PlayerMovement : MonoBehaviour
     public float playerMovementSpeed;
     public float jumpForce;
 
+    public int jumpCount;
+    public int maxJumps = 2;
+
     private bool isGrounded;
     private bool isJumping;
 
-    [SerializeField] private Transform groundCheck;
-
-
+    public Transform groundCheck;           
+    public float groundCheckRadius = 0.2f; 
+    public LayerMask groundLayer;
 
     private void Start()
     {
-        isGrounded = false;
+        isGrounded = true;
         isJumping = false;
     }
 
@@ -29,37 +32,43 @@ public class PlayerMovement : MonoBehaviour
     {
         MovementSystem();
         JumpButton();
-        DoubleJumpButton();
+        DoubleJump();
         WallSlide();
-    
+
+        //if(isGrounded == true)
+        //{
+        //    jumpCount = 0;
+        //}
     }
+
     #region BUTTONS 
     public void MovementSystem()
     {
         rb.velocity = new Vector2(horizontal * playerMovementSpeed, rb.velocity.y);
-
     }
     public void JumpButton()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown("Jump") && isGrounded == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true && jumpCount < maxJumps)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount++;
+            Debug.Log("the jump method has been called");
         }
 
-        if (Input.GetKeyUp("Jump") && rb.velocity.y > 0)
+        if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x * 0.2f, rb.velocity.y * 0.5f);
         }
+   
     }
 
-    public void DoubleJumpButton()
+    public void DoubleJump()
     {
-        if (isGrounded == false && rb.velocity.y > 0)
-        {
 
-        }
+
+
     }
 
     public void WallSlide()
