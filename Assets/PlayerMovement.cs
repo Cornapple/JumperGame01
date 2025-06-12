a2447a683bb843c407625697ad539c53301998fd
DELILAH\corne <1031995@student.sae.edu.au>
2025-06-12 16:03:30 +1000

    commenting through code to outline what every line does

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,29 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour //the movement script made to move the player
 {
-    public GameObject player;
-    public Rigidbody2D rb;
+    public GameObject player; // the in-engine object that the code with move with the below script
+    public Rigidbody2D rb; // the rigidbody (2d physics) applied to the above game object
 
-    private float horizontal;
-    private float vertical;
-    public float playerMovementSpeed;
-    public float jumpForce;
+    private float horizontal; // a number value characterised by point values. in this case refering to the Horizontal axis
+    private float vertical; // a number value characterised by point values. in this case refering to the Vertical axis
+    public float playerMovementSpeed; // a number value characterised by point values. in this case refering to the player movement speed
+    public float jumpForce; // a number value characterised by point values. in this case refering to the jump force of the player
 
-    public int jumpCount;
-    public int maxJumps = 2;
+    public int jumpCount; // a whole number value pertaining to the amount of jumps the player has performed
+    public int maxJumps = 2; // a whole number pertaining to the maximum amount of jumps possible for the player to perform in mid air
 
-    public bool isGrounded;
-    public bool isJumping;
+    public bool isGrounded; // a true or false value. in this case refering to whether the player is touching the ground or not
+    public bool isJumping; // a true or false value. in this case refering to whether the player is jumping or not
 
-    public Transform groundCheck;           
-    public float groundCheckRadius = 0.2f; 
-    public LayerMask groundLayer;
+    public Transform groundCheck;    // empty object with collider to check if the player is touching the ground        
+    public float groundCheckRadius = 0.2f; // the size of the collider used on the groundcheck
+    public LayerMask groundLayer; // the layer mask applied to the ground to allow the groundcheck to function
 
     private void Start()
     {
-        isGrounded = true;
         isJumping = false;
     }
 
@@ -34,20 +33,26 @@ public class PlayerMovement : MonoBehaviour
         JumpButton();
         //DoubleJump();
         WallSlide();
+        CheckIfGrounded();
+
+        //if (isGrounded == true)
+        //{
+        //    isJumping = false;
+        //}
+        //else
+        //{
+        //    isJumping = true;
+        //}
 
         if (isGrounded == true)
         {
+            jumpCount = 0;
             isJumping = false;
         }
-        else if (isGrounded  == false)
+        else
         {
             isJumping = true;
         }
-
-        //if(isGrounded == true)
-        //{
-        //    jumpCount = 0;
-        //}
     }
 
     #region BUTTONS 
@@ -74,6 +79,11 @@ public class PlayerMovement : MonoBehaviour
             return; 
         }
     
+    }
+
+    public void CheckIfGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
     //public void DoubleJump()
