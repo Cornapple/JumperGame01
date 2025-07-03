956e0cb5df81b1f4939de50a5f8902ddb47ea228
Cornapple <1031995@student.sae.edu.au>
2025-07-03 16:25:46 +1000

    began implementing UI and a player stats script

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerStats : MonoBehaviour
+{
+    public int playerHealth;
+    public int playerLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = 5;
+        playerLevel = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
