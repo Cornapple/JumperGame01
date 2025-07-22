f6a3d86e60c83a62d6548c54971fe01a4972adb7
Cornapple <1031995@student.sae.edu.au>
2025-07-22 11:23:30 +1000

    began adding UI elements to screen. importated previous canvas. player is no longer taking damage from enemies not sure why

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class PlayerStats : MonoBehaviour
 {
     public int playerHealth;
     public int maxHealth = 5;
+
+    public Image healthBar;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,8 @@ public class PlayerStats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-   
+        healthBar.fillAmount = Mathf.Clamp(playerHealth / maxHealth, 0, 5);
+
 
     }
 
