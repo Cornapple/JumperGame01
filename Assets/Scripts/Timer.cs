d929f2c068acf62c4c0a1828c7c4f4c653cf43d2
Cornapple <1031995@student.sae.edu.au>
2025-08-03 14:22:37 +1000

    finished colliders. introduced rudimentary timer and countdown timer. when timer finished player respawns

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    public PlayerStats playerStats;
+
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            SceneManager.LoadSceneAsync(1);
+        }
+
+        remainingTime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+        
+    }
+
+    public void TimerRegen()
+    {
+
+    }
+}
